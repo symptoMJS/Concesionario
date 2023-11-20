a1b85601c8e2f393ade6bf4d02ce8747232a5c55
Jaime <68407700+symptoMJS@users.noreply.github.com>
2023-11-20 09:06:44 +0100

    Update Principal.java

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,9 +1,1 @@
-package dominio;
- public static void main(String[] args) {
-        VehiculoTurismo vehiculo1 = new VehiculoTurismo("Marca1", "Modelo1", 10000, 5);
-        System.out.println("Precio final del vehículo 1: " + vehiculo1.calcularPrecioFinal() + " euros");
 
-        VehiculoTurismo vehiculo2 = new VehiculoTurismo("Marca2", "Modelo2", 10000, 7);
-        System.out.println("Precio final del vehículo 2: " + vehiculo2.calcularPrecioFinal() + " euros");
-    }
-}
